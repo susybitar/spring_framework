8babd2351a665a56249a52b20c06c9e380f2f394
susybitar <sbitarazevedo@futurespace.es>
2026-03-24 15:35:11 +0100

    Docs: Añadido JavaDoc y renombrado método a isGreater en Ejercicio 01

--- clean_code/src/exercises/ejercicio01/Ejercicio01.java
+++ clean_code/src/exercises/ejercicio01/Ejercicio01.java
@@ -1,9 +1,20 @@
 package exercises.ejercicio01;
 
+/**
+ * Clase que contiene utilidades lógicas básicas.
+ * Se ha refactorizado para eliminar redundancias booleanas.
+ */
 public class Ejercicio01 {
 
-    // Metodo refactorizado
+    /**
+     * Compara dos numeros enteros para determinar si el primero es estrictamente mayor que el segundo.
+     * * @param a El primer número a comparar.
+     *
+     * @param b El segundo numero a comparar.
+     * @return true si 'a' es mayor que 'b', false en caso contrario.
+     */
     public boolean isGreater(int a, int b) {
+        // Refactorizacion: Se elimina el if/else innecesario y se devuelve directamente la expresion.
         return a > b;
     }
 }
